ee2cf18275062446ffc4775da54976ad58bfc573
Max Walker <maxwell.a.walker@gmail.com>
2016-09-21 14:26:30 -0400

    skip bundling node modules.

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,9 +1,24 @@
 var devConfig = require('./webpack.config.js');
 
+var fs = require('fs');
+
+var nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+
 devConfig.entry = [
   "../src/index.js",
 ]
 
 devConfig.output.libraryTarget = 'commonjs2';
+
+devConfig.target = 'node';
+
+devConfig.externals = nodeModules;
 
 module.exports = devConfig
